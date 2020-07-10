c93e976af8a674346e37b39830fa6baf8ddc538d
Leonardo Macedo <32895268+macedojleo@users.noreply.github.com>
2020-07-09 21:20:20 -0300

    Add files via upload

--- reverse.sh
+++ reverse.sh
@@ -3,14 +3,14 @@
 read -p "Type the string: " string
 
 len=`echo ${#string}`
-invert=""
+reverse=""
 
 i=$(($len - 1));
 while [ $i -ge 0 ]
 do
-   invert+=`echo ${string:$i:1}`
+   reverse+=`echo ${string:$i:1}`
    let i-=1
 done
 
-echo "Reverse string: $invert"
+echo "Reverse string: $reverse"
 
